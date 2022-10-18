996623b8b06f810cd167d538ea4ce2bb948145d8
AntonBush <bushev.anton.ru@yandex.ru>
2022-10-18 18:20:31 +0300

    init

---
+++ lb2_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lb2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        int z =   9 ;
+    }
+
+}
