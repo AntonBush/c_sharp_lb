016d7b2d839a5761a4777d56b34b195f4dc9e7a7
AntonBush <bushev.anton.ru@yandex.ru>
2022-10-26 15:00:35 +0300

    refactor

--- lb2_1/Program.cs
+++ lb2_1/Program.cs
@@ -1,22 +1,78 @@
-﻿using System;
+﻿/*
+
+Задание 1: даны два массива a и b размерностью n и m соответственно,
+сформировать массив c таким образом, что первая часть — отсортированный
+по возрастанию массив а, а вторая часть — отсортированный по
+убыванию массив b.
+
+*/
+
+lb2_1.IntValidator pozitiveInt = (i) => { return 0 < i; };
+lb2_1.IntValidator anyInt = (_) => { return true; };
+
+int n = lb2_1.Console.readInt("Enter n (integer, 0 < n):", pozitiveInt);
+int[] a = new int[n];
+for (int i = 0; i < n; ++i)
+{
+    a[i] = lb2_1.Console.readInt("Enter a[" + i.ToString() + "] (integer):", anyInt);
+}
+
+int m = lb2_1.Console.readInt("Enter m (integer, 0 < m):", pozitiveInt);
+int[] b = new int[m];
+for (int i = 0; i < m; ++i)
+{
+    b[i] = lb2_1.Console.readInt("Enter b[" + i.ToString() + "] (integer):", anyInt);
+}
+
+Console.WriteLine("Entered:");
+writeArray(a, "a");
+writeArray(b, "b");
+
+sortIntArray(a, true);
+sortIntArray(b, false);
+
+Console.WriteLine("Sorted:");
+writeArray(a, "a");
+writeArray(b, "b");
+
+void writeArray(int[] array, string array_name)
+{
+    Console.WriteLine(array_name + "[" + array.Length.ToString() + "] {");
+    for (int i = 0; i < array.Length; ++i)
+    {
+        Console.WriteLine("  " + array_name + "[" + i.ToString() + "] = " + array[i].ToString());
+    }
+    Console.WriteLine("}");
+}
+
+void sortIntArray(int[] array, bool ascending_order)
+{
+    int n_swaps = 1;
+    while (n_swaps != 0)
+    {
+        n_swaps = 0;
+        for (int i = 0; i < array.Length - 1; ++i)
+        {
+            if (ascending_order ? array[i + 1] < array[i] : array[i] < array[i + 1])
+            {
+                int temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+                ++n_swaps;
+            }
+        }
+    }
+}
 
 namespace lb2_1
 {
-    /*
-
-    Задание 1: даны два массива a и b размерностью n и m соответственно,
-    сформировать массив c таким образом, что первая часть — отсортированный
-    по возрастанию массив а, а вторая часть — отсортированный по
-    убыванию массив b.
-
-    */
-
     public delegate bool IntValidator(int i);
-    public class Program
+
+    public class Console
     {
         public static bool readInt(out int i)
         {
-            return int.TryParse(Console.ReadLine(), out i);
+            return int.TryParse(System.Console.ReadLine(), out i);
         }
 
         public static int readInt(string message, IntValidator validateInt)
@@ -25,70 +81,10 @@ namespace lb2_1
 
             do
             {
-                Console.WriteLine(message);
+                System.Console.WriteLine(message);
             } while ((!readInt(out x)) || (!validateInt(x)));
 
             return x;
-        }
-
-        public static void writeArray(int[] array, string array_name)
-        {
-            Console.WriteLine(array_name + "[" + array.Length.ToString() + "] {");
-            for (int i = 0; i < array.Length; ++i)
-            {
-                Console.WriteLine("  " + array_name + "[" + i.ToString() + "] = " + array[i].ToString());
-            }
-            Console.WriteLine("}");
-        }
-
-        public static void sortIntArray(int[] array, bool ascending_order)
-        {
-            int n_swaps = 1;
-            while (n_swaps != 0)
-            {
-                n_swaps = 0;
-                for (int i = 0; i < array.Length - 1; ++i)
-                {
-                    if (ascending_order ? array[i + 1] < array[i] : array[i] < array[i + 1])
-                    {
-                        int temp = array[i];
-                        array[i] = array[i + 1];
-                        array[i + 1] = temp;
-                        ++n_swaps;
-                    }
-                }
-            }
-        }
-
-        static void Main()
-        {
-            IntValidator pozitiveInt = (i) => { return 0 < i; };
-            IntValidator anyInt = (_) => { return true; };
-
-            int n = readInt("Enter n (integer, 0 < n):", pozitiveInt);
-            int[] a = new int[n];
-            for (int i = 0; i < n; ++i)
-            {
-                a[i] = readInt("Enter a[" + i.ToString() + "] (integer):", anyInt);
-            }
-
-            int m = readInt("Enter m (integer, 0 < m):", pozitiveInt);
-            int[] b = new int[m];
-            for (int i = 0; i < m; ++i)
-            {
-                b[i] = readInt("Enter b[" + i.ToString() + "] (integer):", anyInt);
-            }
-
-            Console.WriteLine("Entered:");
-            writeArray(a, "a");
-            writeArray(b, "b");
-
-            sortIntArray(a, true);
-            sortIntArray(b, false);
-
-            Console.WriteLine("Sorted:");
-            writeArray(a, "a");
-            writeArray(b, "b");
         }
     }
 }
--- lb2_2/Program.cs
+++ lb2_2/Program.cs
@@ -1,152 +1,145 @@
-﻿using System;
-using lb2_1;
+﻿/*
+
+Задание 2: создать двумерный массив, размерность задается
+пользователем, заполнить его случайными числами в диапазоне от 0 до 9.
+Отсортировать элементы массива по возрастанию вначале по строкам, а затем
+по столбцам. Вывести на экран исходный массив, массив отсортированный по-
+строчно, массив отсортированный по столбцам.
+
+*/
+
+lb2_1.IntValidator pozitiveInt = (i) => { return 0 < i; };
+lb2_1.IntValidator anyInt = (_) => { return true; };
+
+int n = lb2_1.Console.readInt("Enter n (integer, 0 < n):", pozitiveInt);
+int m = lb2_1.Console.readInt("Enter m (integer, 0 < m):", anyInt);
+int[,] a = new int[n, m];
+
+for (int i = 0; i < n; ++i)
+{
+    for (int j = 0; j < m; ++j)
+    {
+        a[i, j] = lb2_1.Console.readInt("Enter a[" + i.ToString() + ", " + j.ToString() + "] (integer):", anyInt);
+    }
+}
+
+Func<int, int, bool> ascending_order = (lhs, rhs) => { return lhs <= rhs; };
+
+Console.WriteLine("Entered:");
+writeArray(a, "a");
+
+sortIntArrayByRows(a, (lhs, rhs) => { return lhs <= rhs; });
+Console.WriteLine("Sorted by rows:");
+writeArray(a, "a");
+
+sortIntArrayByColumns(a, intLess);
+Console.WriteLine("Sorted by columns:");
+writeArray(a, "a");
+
+sortIntArrayByRows_(a, ascending_order);
+Console.WriteLine("Sorted by rows:");
+writeArray(a, "a");
+
+void writeArray(int[,] array, string array_name)
+{
+    Console.WriteLine($"{array_name}[{array.GetLength(0)}, {array.GetLength(1)}] {{");
+    for (int i = 0; i < array.GetLength(0); ++i)
+    {
+        for (int j = 0; j < array.GetLength(1); ++j)
+        {
+            Console.Write($"  {array_name}[{i}, {j}] = {array[i, j]};");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("}");
+}
+
+void swapInt(ref int lhs, ref int rhs)
+{
+    int temp = lhs;
+    lhs = rhs;
+    rhs = temp;
+}
+
+void sortIntArrayByRows(int[,] array
+                        , lb2_2.IntComparator compare
+                        )
+{
+    int n_rows = array.GetLength(0);
+    int n_columns = array.GetLength(1);
+
+    int n_swaps = 1;
+    while (n_swaps != 0)
+    {
+        n_swaps = 0;
+
+        for (int i = 0; i < n_rows; ++i)
+        {
+            for (int j = 0; j < n_columns - 1; ++j)
+            {
+                if (!compare(array[i, j], array[i, j + 1]))
+                {
+                    swapInt(ref array[i, j], ref array[i, j + 1]);
+                    ++n_swaps;
+                }
+            }
+        }
+    }
+}
+
+void sortIntArrayByRows_(int[,] array
+                , Func<int, int, bool> compare
+                )
+{
+    int n_rows = array.GetLength(0);
+    int n_columns = array.GetLength(1);
+
+    int n_swaps = 1;
+    while (n_swaps != 0)
+    {
+        n_swaps = 0;
+
+        for (int i = 0; i < n_rows; ++i)
+        {
+            for (int j = 0; j < n_columns - 1; ++j)
+            {
+                if (!compare(array[i, j], array[i, j + 1]))
+                {
+                    swapInt(ref array[i, j], ref array[i, j + 1]);
+                    ++n_swaps;
+                }
+            }
+        }
+    }
+}
+
+void sortIntArrayByColumns(int[,] array, lb2_2.IntComparator compare)
+{
+    int n_rows = array.GetLength(0);
+    int n_columns = array.GetLength(1);
+
+    int n_swaps = 1;
+    while (n_swaps != 0)
+    {
+        n_swaps = 0;
+
+        for (int j = 0; j < n_columns; ++j)
+        {
+            for (int i = 0; i < n_rows - 1; ++i)
+            {
+                if (!compare(array[i, j], array[i + 1, j]))
+                {
+                    swapInt(ref array[i, j], ref array[i + 1, j]);
+                    ++n_swaps;
+                }
+            }
+        }
+    }
+}
+
+bool intLess(int lhs, int rhs) { return lhs <= rhs; }
 
 namespace lb2_2
 {
-    /*
-
-    Задание 2: создать двумерный массив, размерность задается
-    пользователем, заполнить его случайными числами в диапазоне от 0 до 9.
-    Отсортировать элементы массива по возрастанию вначале по строкам, а затем
-    по столбцам. Вывести на экран исходный массив, массив отсортированный по-
-    строчно, массив отсортированный по столбцам.
-
-    */
     public delegate bool IntComparator(int lhs, int rhs);
-    public class Program
-    {
-        public static void writeArray(int[,] array, string array_name)
-        {
-            Console.WriteLine($"{array_name}[{array.GetLength(0)}, {array.GetLength(1)}] {{");
-            for (int i = 0; i < array.GetLength(0); ++i)
-            {
-                for (int j = 0; j < array.GetLength(1); ++j)
-                {
-                    Console.Write($"  {array_name}[{i}, {j}] = {array[i, j]};");
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("}");
-        }
-
-        public static void swapInt(ref int lhs, ref int rhs)
-        {
-            int temp = lhs;
-            lhs = rhs;
-            rhs = temp;
-        }
-
-        public static void sortIntArrayByRows(int[,] array
-                                , IntComparator compare
-                                )
-        {
-            int n_rows = array.GetLength(0);
-            int n_columns = array.GetLength(1);
-
-            int n_swaps = 1;
-            while (n_swaps != 0)
-            {
-                n_swaps = 0;
-
-                for (int i = 0; i < n_rows; ++i)
-                {
-                    for (int j = 0; j < n_columns - 1; ++j)
-                    {
-                        if (!compare(array[i, j], array[i, j + 1]))
-                        {
-                            swapInt(ref array[i, j], ref array[i, j + 1]);
-                            ++n_swaps;
-                        }
-                    }
-                }
-            }
-        }
-
-        public static void sortIntArrayByRows_(int[,] array
-                        , Func<int, int, bool> compare
-                        )
-        {
-            int n_rows = array.GetLength(0);
-            int n_columns = array.GetLength(1);
-
-            int n_swaps = 1;
-            while (n_swaps != 0)
-            {
-                n_swaps = 0;
-
-                for (int i = 0; i < n_rows; ++i)
-                {
-                    for (int j = 0; j < n_columns - 1; ++j)
-                    {
-                        if (!compare(array[i, j], array[i, j + 1]))
-                        {
-                            swapInt(ref array[i, j], ref array[i, j + 1]);
-                            ++n_swaps;
-                        }
-                    }
-                }
-            }
-        }
-
-        public static void sortIntArrayByColumns(int[,] array, IntComparator compare)
-        {
-            int n_rows = array.GetLength(0);
-            int n_columns = array.GetLength(1);
-
-            int n_swaps = 1;
-            while (n_swaps != 0)
-            {
-                n_swaps = 0;
-
-                for (int j = 0; j < n_columns; ++j)
-                {
-                    for (int i = 0; i < n_rows - 1; ++i)
-                    {
-                        if (!compare(array[i, j], array[i + 1, j]))
-                        {
-                            swapInt(ref array[i, j], ref array[i + 1, j]);
-                            ++n_swaps;
-                        }
-                    }
-                }
-            }
-        }
-
-        static void Main()
-        {
-            IntValidator pozitiveInt = (i) => { return 0 < i; };
-            IntValidator anyInt = (_) => { return true; };
-
-            int n = lb2_1.Program.readInt("Enter n (integer, 0 < n):", pozitiveInt);
-            int m = lb2_1.Program.readInt("Enter m (integer, 0 < m):", anyInt);
-            int[,] a = new int[n, m];
-
-            for (int i = 0; i < n; ++i)
-            {
-                for (int j = 0; j < m; ++j)
-                {
-                    a[i, j] = lb2_1.Program.readInt("Enter a[" + i.ToString() + ", " + j.ToString() + "] (integer):", anyInt);
-                }
-            }
-
-            Func<int, int, bool> ascending_order = (lhs, rhs) => { return lhs <= rhs; };
-
-            Console.WriteLine("Entered:");
-            writeArray(a, "a");
-
-            sortIntArrayByRows(a, (lhs, rhs) => { return lhs <= rhs; });
-            Console.WriteLine("Sorted by rows:");
-            writeArray(a, "a");
-
-            sortIntArrayByColumns(a, intLess);
-            Console.WriteLine("Sorted by columns:");
-            writeArray(a, "a");
-
-            sortIntArrayByRows_(a, ascending_order);
-            Console.WriteLine("Sorted by rows:");
-            writeArray(a, "a");
-        }
-
-        static bool intLess(int lhs, int rhs) { return lhs <= rhs; }
-    }
 }
